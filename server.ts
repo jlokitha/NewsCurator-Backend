9b41623b110e929581998f7ed2c4704c58e02550
jlokitha <lokithajanindu040130@gmail.com>
2025-02-28 19:41:05 +0530

    project intialization

---
+++ server.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+// @ts-ignore
+import cors from "cors";
+import dotenv from 'dotenv';
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+}));
+
+app.listen(5000, (err) => {
+    console.log('Server is running on port 5000');
+});
